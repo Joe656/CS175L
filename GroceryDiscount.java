85f99bde117f111e9772c7d4d6775244830c15b7
Joe656 <55414247+Joe656@users.noreply.github.com>
2020-02-17 16:03:11 -0500

    Add files via upload

---
+++ GroceryDiscount.java
@@ -0,0 +1,37 @@
+package housePaintCost;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class GroceryDiscount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ boolean start = true; 
+ Scanner scan = new Scanner(System.in);
+ DecimalFormat df = new DecimalFormat("0.00");
+ df.setMaximumFractionDigits(2);
+ 
+while (start = true) {
+	System.out.println("Please enter the cost of groceries or '0' to end the program. ");
+	 double groceryCost = scan.nextDouble();
+if (groceryCost == 0) {
+	start = false; 
+	return;
+}
+else if (groceryCost > 210) {
+	System.out.println("You won a discount coupon of: "+df.format(groceryCost*.14)+(" 14% of your purchase"));
+}
+else if (groceryCost > 150 ) {
+	System.out.println("You won a discount coupon of: "+df.format(groceryCost*.12)+(" 12% of your purchase"));
+}
+else if (groceryCost > 60 ) {
+	System.out.println("You won a discount coupon of: "+df.format(groceryCost*.10)+(" 10% of your purchase"));
+}
+else if (groceryCost >= 10) {
+	System.out.println("You won a discount coupon of: "+ df.format(groceryCost*.08)+(" 8% of your purchase"));
+}
+else
+	System.out.println("You are not eligible for a dicount code");
+}
+	}
+
+}
